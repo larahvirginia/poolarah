60b5c24d5b660da663d62b84badb55be097f4d2a
larahvirginia <larahv18@hotmail.com>
2020-12-12 19:58:03 -0300

    Create Lapiseira.java

---
+++ projeto 03 cofre/Lapiseira.java
@@ -0,0 +1,57 @@
+package lapiseira;
+
+class Grafite {
+    float calibre;
+    String dureza;
+    int tamanho;
+    
+    public Grafite (float calibre, String dureza, int tamanho) {
+        this.calibre = calibre;
+        this.dureza = dureza;
+        this.tamanho = tamanho;
+    }
+    
+    public String toString() {
+        return " [" + calibre + ":" + dureza + ":" + tamanho + "]";
+    }
+}
+
+public class Lapiseira {
+    float calibre;
+    Grafite grafite;
+    
+    Lapiseira (float calibre) {
+        this.calibre = calibre;
+        this.grafite = null;
+    }
+    
+    void inserir (Grafite grafite) {
+        if (this.grafite != null) {
+            System.out.println("já exite grafite");
+        } else if (this.calibre < grafite.calibre) {
+            System.out.println("calibre incompatível");
+        } else {
+            this.grafite = grafite;
+        }
+    }
+    
+    void remover () {
+        if (this.grafite != null) {
+            this.grafite = null;
+        }
+    }
+    
+    void escrever (int folhas) {
+        
+    }
+    
+    public String toString() {
+        return "calibre:" + calibre + "," + " grafite:" + grafite;
+    }
+    
+    public static void main(String[] args) {
+        Lapiseira lapiseira = new Lapiseira(0.5f);
+        System.out.println(lapiseira);
+        lapiseira.inserir(new Grafite(0.5f, "HB", 4));
+    }
+}
